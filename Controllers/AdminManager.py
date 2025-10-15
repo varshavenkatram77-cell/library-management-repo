ea8977a4db1387a8898c2727d8d071d6689eb230
varshavenkatram77-cell <varshavenkatram77@gmail.com>
2025-10-15 13:35:41 +0530

    Update AdminManager.py

--- Controllers/AdminManager.py
+++ Controllers/AdminManager.py
@@ -1,36 +1,24 @@
-from App.Admin import Admin
+from App.Admin import  Admin
 
-class AdminManager():
-	def __init__(self, DAO):
-		self.admin = Admin(DAO.db.admin)
-		self.user = DAO.db.user
-		self.dao = self.admin.dao
-
-	def signin(self, email, password):
-		admin = self.dao.getByEmail(email)
-
-		if admin is None:
-			return False
-
-		admin_pass = admin["password"] # admin pass at 
-		if admin_pass != password:
-			return False
-
-		return admin
-		
-	def get(self, id):
-		admin = self.dao.getById(id)
-
-		return admin
-		
-	def getUsersList(self):
-		admin = self.user.list()
-		print(admin)
-
-		return admin
-
-	def signout(self):
-		self.admin.signout()
-
-	def user_list(self):
-		return self.user.list()
+class adminmanager:
+  def __init__ ( self ,DAO ):
+     self.admin=Admin(DAO.db.admin)
+     self.user=DAO.db.user
+     self.dao=self.admin.dao
+  def signin ( self,email , password ):
+        admin=self.dao.getByEmail(email)
+        if(admin==None):
+           return  False
+        admin_pass=admin["password"]
+        if admin_pass!=password:return False
+        return admin
+  def get(self,ID ):
+          admin=self.dao.getById(ID)
+          return admin
+  def getUsersList( self ):
+     admin=self.user.list()
+     print( admin )
+     return  admin
+  def signout( self ): self.admin.signout()
+  def user_list( self ):
+         return self.user.list()
